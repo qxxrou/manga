c45f34ad685b74160440284a7d20ba3278967ea6
xinrou <wugaoweia@gmail.com>
2025-08-30 23:34:53 +0800

    2

--- server/src/home/home.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common'
-import { HomeController } from './home.controller.js'
-import { HomeService } from './home.service.js'
-
-@Module({
-  controllers: [HomeController],
-  providers: [HomeService],
-})
-export class HomeModule {}
--- server/src/puppeteer/puppeteer.controller.ts
+++
@@ -1,6 +0,0 @@
-import { Controller } from '@nestjs/common'
-
-@Controller('puppeteer')
-export class PuppeteerController {
-  constructor() {}
-}
